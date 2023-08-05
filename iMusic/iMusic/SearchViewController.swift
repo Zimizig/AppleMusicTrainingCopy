e300cd0e4d3d6439abdbc84c6e630cbbd7f39d6a
Zimizig <ramazanow9999@gmail.com>
2023-08-05 18:28:12 +0500

    Cocoapods & Alamofire

--- iMusic/iMusic/SearchViewController.swift
+++ iMusic/iMusic/SearchViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct TrackModel {
     var trackName: String
@@ -14,6 +15,7 @@ struct TrackModel {
 
 class SearchViewController: UITableViewController {
     
+    let searchController = UISearchController(searchResultsController: nil)
     let trackList = [
                   TrackModel(trackName: "First track", artistName: "Noname1"),
                   TrackModel(trackName: "First track", artistName: "Noname1"),
@@ -38,9 +40,17 @@ class SearchViewController: UITableViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .darkGray
-        
+        setupSearchBar()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ID")
         
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.searchController?.searchBar.placeholder = "Артисты, песни, тексты и др."
+        navigationItem.searchController?.searchBar.tintColor = .systemPink
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -59,4 +69,20 @@ class SearchViewController: UITableViewController {
         return cell
     }
         
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        AF.request(url).responseData { dataResponse in
+            if let error = dataResponse.error {
+                print("Error received requesting data: \(error.localizedDescription)")
+                return
+            }
+            guard let data = dataResponse.data else { return }
+            let someString = String(data: data, encoding: .utf8)
+            print(someString ?? "")
+        }
+    }
 }
