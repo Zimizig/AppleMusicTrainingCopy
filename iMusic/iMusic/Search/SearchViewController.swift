de590728da867cca111880435377a7a1d0b03546
Zimizig <ramazanow9999@gmail.com>
2023-08-09 08:09:47 +0500

    SOLID refactoring & XIB work

--- iMusic/iMusic/Search/SearchViewController.swift
+++ iMusic/iMusic/Search/SearchViewController.swift
@@ -59,6 +59,9 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     
     private func setupTableView() {
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        let nib = UINib(nibName: "TrackCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: TrackCell.reuseId)
     }
     
     func displayData(viewModel: Search.Model.ViewModel.ViewModelData) {
@@ -78,19 +81,25 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
 // MARK: - UITableViewDelegate, UITableViewDataSource
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchViewModel.cells.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = table.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let cell = table.dequeueReusableCell(withIdentifier: TrackCell.reuseId, for: indexPath) as! TrackCell
         
         let cellViewModel = searchViewModel.cells[indexPath.row]
-        cell.textLabel?.text = "\(cellViewModel.trackName)\n\(cellViewModel.artistName)"
-        cell.textLabel?.numberOfLines = 2
-        cell.imageView?.image = UIImage(named: "pop3")
+        
+        cell.imageView?.backgroundColor = .systemRed
+        cell.set(viewModel: cellViewModel)
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 84
+    }
+    
 }
 
 extension SearchViewController: UISearchBarDelegate {
